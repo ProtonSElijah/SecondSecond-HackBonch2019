fae548f3e00769338c4d24b3ac9f6cbfac55c4de
ProtonSElijah <SElijah@protonmail.com>
2019-12-03 06:22:00 +0300

    Добавлен выбор размера

--- src/AppSecondSecond.js
+++ src/AppSecondSecond.js
@@ -3,8 +3,22 @@ import connect from '@vkontakte/vk-connect';
 import View from '@vkontakte/vkui/dist/components/View/View';
 import ModalRoot from '@vkontakte/vkui/dist/components/ModalRoot/ModalRoot';
 
+import { IS_PLATFORM_ANDROID, IS_PLATFORM_IOS } from '@vkontakte/vkui/dist/lib/platform';
+import ModalPage from '@vkontakte/vkui/dist/components/ModalPage/ModalPage';
+import ModalPageHeader from '@vkontakte/vkui/dist/components/ModalPageHeader/ModalPageHeader';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout';
+import Checkbox from '@vkontakte/vkui/dist/components/Checkbox/Checkbox';
+import RangeSlider from '@vkontakte/vkui/dist/components/RangeSlider/RangeSlider';
+import SelectMimicry from '@vkontakte/vkui/dist/components/SelectMimicry/SelectMimicry';
+import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
+import Icon24Done from '@vkontakte/icons/dist/24/dismiss';
+import Select from '@vkontakte/vkui/dist/components/Select/Select';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import List from '@vkontakte/vkui/dist/components/List/List';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+
 import Main from './panels/Main';
-import Filters from './modals/Filters';
 import ProductInfo from './modals/ProductInfo';
 
 import '@vkontakte/vkui/dist/vkui.css';
@@ -28,6 +42,10 @@ const AppSecondSecond = () => {
 
     const MODAL_PAGE_PRODUCTINFO = "product-info";
     const MODAL_PAGE_FILTER = "filter";
+    const MODAL_PAGE_SIZE = "size";
+    const MODAL_PAGE_STORE = "store";
+
+    const [sizes, setSizes] = useState(["XXS", "XS", "S", "M", "L", "XL", "XXL"]);
 
     const LOCAL_SERVER = "192.168.0.106";
 
@@ -91,6 +109,24 @@ const AppSecondSecond = () => {
         setActiveModal(MODAL_PAGE_PRODUCTINFO);
     };
 
+    const toSize = e => {setActiveModal(MODAL_PAGE_SIZE)};
+    const toFilter = e => {setActiveModal(MODAL_PAGE_FILTER)};
+
+    const switching = e => {
+        let s = sizes.slice();
+        if (s.indexOf(e.currentTarget.dataset.size) != -1) s.splice(s.indexOf(e.currentTarget.dataset.size), 1);
+        else s.push(e.currentTarget.dataset.size);
+        let newS = [];
+        if (s.indexOf("XXS") != -1) newS.push("XXS");
+        if (s.indexOf("XS") != -1) newS.push("XS");
+        if (s.indexOf("S") != -1) newS.push("S");
+        if (s.indexOf("M") != -1) newS.push("M");
+        if (s.indexOf("L") != -1) newS.push("L");
+        if (s.indexOf("XL") != -1) newS.push("XL");
+        if (s.indexOf("XXL") != -1) newS.push("XXL");
+        setSizes(newS);
+    };
+
     const modal = (
       <ModalRoot activeModal={activeModal}>
        <ProductInfo
@@ -103,13 +139,97 @@ const AppSecondSecond = () => {
             url={urlProductModal}
             price={priceProductModal}
             id={MODAL_PAGE_PRODUCTINFO}/>
-        <Filters
-            onClose={modalBack}
-            modalBack={modalBack}
-            onChangePrice={onChangePrice}
-            minPriceChange={minPriceChange}
-            maxPriceChange={maxPriceChange}
-            id={MODAL_PAGE_FILTER}/>
+
+        <ModalPage
+          id={MODAL_PAGE_FILTER}
+          onClose={modalBack}
+          settlingHeight={300}
+          header={
+            <ModalPageHeader
+              right={<HeaderButton onClick={modalBack}>{IS_PLATFORM_IOS ? 'Готово' : <Icon24Done />}</HeaderButton>}>
+              Критерии поиска
+            </ModalPageHeader>
+          }>
+          <FormLayout>
+            <RangeSlider onChange={onChangePrice}
+                top={"Ценовой диапазон: " + minPriceChange + " - " + maxPriceChange}
+                min={0}
+                max={12000}
+                step={1}
+                defaultValue={[0, 12000]}/>
+              <SelectMimicry
+                  top="Выберите размеры"
+                  placeholder={sizes.toString()}
+                  onClick={toSize}>
+                </SelectMimicry>
+                <SelectMimicry
+                  top="Выберите магазины"
+                  placeholder={sizes.toString()}
+                  onClick={toSize}>
+                </SelectMimicry>
+          </FormLayout>
+        </ModalPage>
+
+        <ModalPage
+          id={MODAL_PAGE_SIZE}
+          onClose={toFilter}
+          settlingHeight={300}
+          header={
+            <ModalPageHeader
+              right={<HeaderButton onClick={toFilter}>{IS_PLATFORM_IOS ? 'Готово' : <Icon24Done />}</HeaderButton>}>
+              Размер одежды
+            </ModalPageHeader>
+          }>
+          <FormLayout>
+            <Group>
+                <List>
+                  <Cell
+                   data-size="XXS"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("XXS") != -1) ? "Добавлено" : ""}>
+                    XXS
+                  </Cell>
+                  <Cell
+                   data-size="XS"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("XS") != -1) ? "Добавлено" : ""}>
+                    XS
+                  </Cell>
+                  <Cell
+                   data-size="S"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("S") != -1) ? "Добавлено" : ""}>
+                    S
+                  </Cell>
+                  <Cell
+                   data-size="M"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("M") != -1) ? "Добавлено" : ""}>
+                    M
+                  </Cell>
+                  <Cell
+                   data-size="L"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("L") != -1) ? "Добавлено" : ""}>
+                    L
+                  </Cell>
+                  <Cell
+                   data-size="XL"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("XL") != -1) ? "Добавлено" : ""}>
+                    XL
+                  </Cell>
+                  <Cell
+                    data-size="XXL"
+                    onClick={switching}
+                    asideContent={(sizes.indexOf("XXL") != -1) ? "Добавлено" : ""}>
+                    XXL
+                  </Cell>
+                </List>
+              </Group>
+          </FormLayout>
+        </ModalPage>
+
       </ModalRoot>
     );
 
--- src/modals/Filters.js
+++ src/modals/Filters.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { IS_PLATFORM_ANDROID, IS_PLATFORM_IOS } from '@vkontakte/vkui/dist/lib/platform';
 import ModalPage from '@vkontakte/vkui/dist/components/ModalPage/ModalPage';
 import ModalPageHeader from '@vkontakte/vkui/dist/components/ModalPageHeader/ModalPageHeader';
@@ -6,18 +6,22 @@ import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderBut
 import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout';
 import Checkbox from '@vkontakte/vkui/dist/components/Checkbox/Checkbox';
 import RangeSlider from '@vkontakte/vkui/dist/components/RangeSlider/RangeSlider';
+import SelectMimicry from '@vkontakte/vkui/dist/components/SelectMimicry/SelectMimicry';
 import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 import Icon24Done from '@vkontakte/icons/dist/24/dismiss';
+import Select from '@vkontakte/vkui/dist/components/Select/Select';
 
 
-const Filters = ({modalBack, onChangePrice, minPriceChange, maxPriceChange, id}) => (
+const Filters = ({modalBack, onChangePrice, minPriceChange, maxPriceChange, id}) => {
+    const [sizes, setSizes] = useState(["XXS", "XS", "S", "M", "L", "XL", "XXL"]);
+
+    return (
         <ModalPage
           id={id}
           onClose={modalBack}
           settlingHeight={300}
           header={
             <ModalPageHeader
-              left={IS_PLATFORM_ANDROID && <HeaderButton onClick={modalBack}><Icon24Cancel /></HeaderButton>}
               right={<HeaderButton onClick={modalBack}>{IS_PLATFORM_IOS ? 'Готово' : <Icon24Done />}</HeaderButton>}>
               Критерии поиска
             </ModalPageHeader>
@@ -28,22 +32,17 @@ const Filters = ({modalBack, onChangePrice, minPriceChange, maxPriceChange, id})
                 min={0}
                 max={12000}
                 step={1}
-                defaultValue={[0, 12000]}
-              />
-              <div className="sizeCheckbox">
-                  <Checkbox name="size" value="XXS">XXS</Checkbox>
-                  <Checkbox name="size" value="XS">XS</Checkbox>
-                  <Checkbox name="size" value="S">S</Checkbox>
-                  <Checkbox name="size" value="M">M</Checkbox>
-              </div>
-                <div className="sizeCheckbox">
-                  <Checkbox name="size" value="L">L</Checkbox>
-                  <Checkbox name="size" value="XL">XL</Checkbox>
-                  <Checkbox name="size" value="XXL">XXL</Checkbox>
-              </div>
+                defaultValue={[0, 12000]}/>
+              <SelectMimicry
+                  top="Выберите размеры"
+                  placeholder="Не выбрана"
+                  onClick={alert("s")}>
+
+                </SelectMimicry>
           </FormLayout>
 
         </ModalPage>
     );
+}
 
 export default Filters;
